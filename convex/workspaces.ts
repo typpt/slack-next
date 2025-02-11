c5a36c699203ce8c12836710a16c19bdbc370f34
Krisno Mukti <typescript97@gmail.com>
2025-02-11 16:31:17 +0700

    feat(workspaces/server): added create workspaces server

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,29 @@
-import { query } from './_generated/server';
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
+import { getAuthUserId } from '@convex-dev/auth/server';
+
+export const createWorkspaces = mutation({
+  args: { name: v.string() },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    function generateRandomNumber() {
+      return Math.floor(10000 + Math.random() * 90000);
+    }
+
+    const workspaceId = await ctx.db.insert('workspaces', {
+      name: args.name,
+      userId: userId,
+      joinCode: String(generateRandomNumber()),
+    });
+
+    return workspaceId;
+  },
+});
 
 export const getWorkspaces = query({
   args: {},
